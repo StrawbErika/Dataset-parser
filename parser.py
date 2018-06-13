fdd11d50e6ab064eb0d9db553427d3a6587b4b85
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-06-13 17:14:30 +0800

    + gets road_data

--- parser.py
+++ parser.py
@@ -4,9 +4,9 @@ import json
 def ask_direction():
     direction = input("Northbound, Southbound? ")
     if(direction.lower() == "northbound"):
-        direction = "Northbound"
+        direction = "northbound"
     elif(direction.lower() == "southbound"):
-        direction = "Southbound"
+        direction = "southbound"
     else:
         print("error direction")
     return direction
@@ -15,28 +15,27 @@ def get_files(directory):
     return os.listdir(directory)
     
 def ask_main_road():
-    main_road = input("EDSA\n Commonwealth \n Quezon ave\n Espana\n C5\n Ortigas\n Marcos highway\n Roxas blvd\n SLEX\n  ")
-    if(direction.lower() == "edsa"):
-        direction = "EDSA"
-    elif(direction.lower() == "commonwealth"):
-        direction = "COMMONWEALTH"
-    elif(direction.lower() == "quezon ave"):
-        direction = "QUEZON"
-    elif(direction.lower() == "espana"):
-        direction = "ESPAA"
-    elif(direction == "C5"):
-        direction = "C5"
-    elif(direction.lower() == "ortigas"):
-        direction = "ORTIGAS"
-    elif(direction.lower() == "marcos highway"):
-        direction = "MARCOS"
-    elif(direction.lower() == "roxas blvd"):
-        direction = "ROXAS"
-    elif(direction.lower() == "slex"):
-        direction = "SLEX"
-    else:
-        print("error main road")
-    return main_road
+    mr = input("[1] EDSA\n [2] Commonwealth \n [3] Quezon ave\n [4] Espana\n [5] C5\n [6] Ortigas\n [7] Marcos highway\n [8] Roxas blvd\n [9] SLEX\n  ")
+    road = ""
+    if(mr == "1"):
+        road = "EDSA"
+    elif(road == "2"):
+        road = "COMMONWEALTH"
+    elif(road == "3"):
+        road = "QUEZON"
+    elif(road == "4"):
+        road = "ESPAA"
+    elif(road == "5"):
+        road = "C5"
+    elif(road == "6"):
+        road = "ORTIGAS"
+    elif(road == "7"):
+        road = "MARCOS"
+    elif(road == "8"):
+        road = "ROXAS"
+    elif(road == "9"):
+        road = "SLEX"
+    return road
 
 def load_file_into_dict(file_name):
     file = open(file_name, "r") 
@@ -46,6 +45,7 @@ def load_file_into_dict(file_name):
     return contents_dict
 
 def road_names(main_road, road_data):
+    print(main_road)
     list_road_names = []
     num = 0
     x = 0
@@ -54,9 +54,29 @@ def road_names(main_road, road_data):
         if(complete_name[0] == main_road):
             list_road_names.append(road_data[num]["line"])
             print("[" + str(x) + "] " +road_data[num]["line"])
-        x = x + 1
+            x = x + 1
         num = num + 1
     return list_road_names
 
+def ask_sub_road():
+    sub = input("Choose from above:")
+    return int(sub)
+
+def find_road_data_index(complete_road_data, name):
+    num = 0
+    while(num != len(complete_road_data)):
+        if(complete_road_data[num]["line"] == name):
+            print(num)
+            index = num 
+        num = num + 1
+    return index
+
+
 complete_road_data = load_file_into_dict("/home/shortcake/Desktop/OJT/Dataset-parser/out/traffic-status-20180530000000.json")
-road_names("COMMONWEALTH", complete_road_data)
+main_road = ask_main_road()
+direction =  ask_direction()
+sub_roads = road_names(main_road, complete_road_data)
+sub_road_index = ask_sub_road()
+chosen_road = sub_roads[sub_road_index]
+road_data = complete_road_data[find_road_data_index(complete_road_data,chosen_road)]
+print(road_data)
